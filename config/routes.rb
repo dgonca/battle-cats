1c27427d9d0b735c97a58258d94ac2bf1d83d4e7
Saham Khozestani <email": null>
2017-08-18 12:10:02 -0500

    add pets controller to app/controllers, add pets index resources to routes.rb in config dir, add rspec tests to rspec/controllers/pets_controller_spec, add index get to the pets_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :pets, only: [:index, :new, :create]
   root to: "sessions#new"
 end
---
+++ spec/controllers/pets_controller_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe PetsController, type: :controller do
+  let!(:user) {User.create!(email: "saham@att.net", password: "test")}
+  let!(:pet) {Pet.create(name: "Zee", animal_type: "Zee", bio: "a cute Zee", zipcode: "60192", cuteness: 10)}
+  before(:each) do
+    session[:user_id] = 1
+  end
+
+  it "has a 200 status code" do
+    get :index
+    expect(response.status).to eq (200)
+  end
+
+  it "has a collection of pets objects" do
+    get :index
+    expect(assigns(:pets)).to all(be_a Pet)
+  end
+
+  it "renders the index template" do
+    get :index
+    expect(response).to render_template("index")
+  end
+end
