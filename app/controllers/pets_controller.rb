0822945cbe4c40f2c99ab503750d774d6f878642
Brendan O'Handley <brendan.ohandley@gmail.com>
2017-08-18 15:43:20 -0500

    pet create controller, view, handle errors, add paperclip gem

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,3 +1,23 @@
 class PetsController < ApplicationController
 
+  def new
+    @user = User.find_by(id: params[:user_id])
+    @pet = Pet.new
+  end
+
+  def create
+    @user = User.find_by(id: params[:user_id])
+    @pet = @user.pets.build(pet_params)
+
+    if @pet.save
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
+
+  private
+    def pet_params
+      params.require(:pet).permit(:avatar, :name, :animal_type, :bio, :zipcode)
+    end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,11 @@
 class Pet < ApplicationRecord
-	has_many :pet_battles
+	
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  validates_attachment_size :avatar, :in => 0.megabytes..1.megabytes
+
+
+  has_many :pet_battles
 	has_many :battles, through: :pet_battles
 	belongs_to :owner, class_name: "User"
 
