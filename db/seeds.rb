c68139b664ad7128e51d2cad2fcff6dfb24811ca
Brendan O'Handley & Saham Khozestani <pair+bohandley.sahamak@devbootcamp.com>
2017-08-18 16:30:29 -0500

    add seed images for pets

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ users = User.create([
 	{email: "ehuggi@saic.edu", password: "password"},
 	{email: "osbornegabriel@gmail.com", password: "password"},
 	{email: "qymanab@gmail.com", password: "password"}
-	])	
+	])
 
 dom = User.find_by(email: "domstar1595@yahoo.com")
 mark = User.find_by(email: "mdh.bing@gmail.com")
@@ -22,12 +22,12 @@ gabe = User.find_by(email: "osbornegabriel@gmail.com")
 qymana = User.find_by(email: "qymanab@gmail.com")
 
 pets = Pet.create([
-	{name: "Oz", animal_type: "cat", zipcode: 60647, owner: dom, bio: "the sweetest cuddle cat who loves to play fetch and bat you awake to feed him."},
-	{name: "Basil", animal_type: "snake", zipcode: 60647, owner: dom, bio: "a slitherly snek who loves to give hand hugs with his body"},
-	{name: "Lambchop", animal_type: "cat", zipcode: 60625, owner: mark, bio: "lightfooted statuesque angel who always catches her prey."},
-	{name: "Luna", animal_type: "cat", zipcode: 60625, owner: mark, bio: "a beautiful girl with a petite frame and a frosty face."},
-	{name: "Spooky", animal_type: "rabbit", zipcode: 60647, owner: kai, bio: "speed racer who loves to nibble on everything and be cuddled while upside down in your arms"},
-	{name: "Sir Fluffly Claws", animal_type: "owlbear", zipcode: 60608, owner: gabe, bio: "Fluffy enjoys eating mice and winter hibernation. You'd think Chicago would be the right place for him, but there aren't that many local caves."},
-	{name: "Humphrey", animal_type: "dog", zipcode: 60601, owner: qymana, bio: "It's Humph! He like fries and making grumble noises when you pet him!"}
+	{name: "Oz", animal_type: "cat", zipcode: 60647, owner: dom, bio: "the sweetest cuddle cat who loves to play fetch and bat you awake to feed him.", avatar: File.open('/Users/apprentice/battle-cats/db/cat1.jpg')},
+	{name: "Basil", animal_type: "snake", zipcode: 60647, owner: dom, bio: "a slitherly snek who loves to give hand hugs with his body", avatar: File.open('/Users/apprentice/battle-cats/db/cat2.jpg')},
+	{name: "Lambchop", animal_type: "cat", zipcode: 60625, owner: mark, bio: "lightfooted statuesque angel who always catches her prey.", avatar: File.open('/Users/apprentice/battle-cats/db/cat3.jpg')},
+	{name: "Luna", animal_type: "cat", zipcode: 60625, owner: mark, bio: "a beautiful girl with a petite frame and a frosty face.", avatar: File.open('/Users/apprentice/battle-cats/db/cat4.jpg')},
+	{name: "Spooky", animal_type: "rabbit", zipcode: 60647, owner: kai, bio: "speed racer who loves to nibble on everything and be cuddled while upside down in your arms", avatar: File.open('/Users/apprentice/battle-cats/db/cat5.jpg')},
+	{name: "Sir Fluffly Claws", animal_type: "owlbear", zipcode: 60608, owner: gabe, bio: "Fluffy enjoys eating mice and winter hibernation. You'd think Chicago would be the right place for him, but there aren't that many local caves.", avatar: File.open('/Users/apprentice/battle-cats/db/cat6.jpg')},
+	{name: "Humphrey", animal_type: "dog", zipcode: 60601, owner: qymana, bio: "It's Humph! He like fries and making grumble noises when you pet him!", avatar: File.open('/Users/apprentice/battle-cats/db/cat7.jpg')}
 	])
-	
+
