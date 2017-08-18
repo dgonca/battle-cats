b83b7d079dab4f4b1e8ffaaabdfc4ac50fcee125
dgonca <domstar1595@yahoo.com>
2017-08-18 12:25:44 -0500

    setting up users login, users path and linking pages to others

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,3 +1,15 @@
 class PetsController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+	def show
+	end
+
+	private
+		def set_pet
+			@pet = Pet.find(params[:id])
+		end
+
+		# def pet_params
+		# 	params.require(:pet).permit(:name, :bio, :email, :password)
+		# end
 end
