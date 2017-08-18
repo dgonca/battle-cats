33dd36f75bcc937ae84692c6ec8ad274ad429b86
mwmayerle <mwmayerle@gmail.com>
2017-08-18 15:41:37 -0500

    removed personal email from rspec test files

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
   resources :users
-=======
+
   resources :pets, only: [:index, :new, :create]
   resources :users do 
   	resources :pets
@@ -10,7 +9,5 @@ Rails.application.routes.draw do
   
   resources :sessions
 
->>>>>>> a666c7dfc71bc4fbea6a72dcd04661f564987b73
   root to: "sessions#new"
-
 end
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe SessionsController, type: :controller do
+  let!(:user) {User.create!(email: "matt@matt.com", password: "mattmatt")}
+
+  before(:each) do
+    session[:user_id] = 1
+  end
+
+  it "has a 200 status code" do
+    get :new
+    expect(response.status).to eq (200)
+  end
+
+  it "renders the new template" do
+    get :new
+    expect(response).to render_template("new")
+  end
+end
