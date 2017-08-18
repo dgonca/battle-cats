51a8afdfda257e0cfc2b05d8b5a55a191a0e8d5d
Brendan O'Handley & Saham Khozestani <pair+bohandley.sahamak@devbootcamp.com>
2017-08-18 16:50:25 -0500

    add rspec test to test the new pet in pets_controller_spec to render a template, creat a new pet

--- spec/controllers/pets_controller_spec.rb
+++ spec/controllers/pets_controller_spec.rb
@@ -3,23 +3,41 @@ require "rails_helper"
 
 RSpec.describe PetsController, type: :controller do
   let!(:user) {User.create!(email: "saham@att.net", password: "test")}
-  let!(:pet) {Pet.create(name: "Zee", animal_type: "Zee", bio: "a cute Zee", zipcode: "60192", cuteness: 10)}
+  let!(:pet) {Pet.create(name: "Zee", animal_type: "Zee", bio: "a cute Zee", zipcode: "60192", cuteness: 10, avatar: File.open('/Users/apprentice/battle-cats/db/cat1.jpg'))}
   before(:each) do
     session[:user_id] = 1
   end
+  describe "Get # index" do
+    it "has a 200 status code" do
+      get :index
+      expect(response.status).to eq (200)
+    end
 
-  it "has a 200 status code" do
-    get :index
-    expect(response.status).to eq (200)
+    it "has a collection of pets objects" do
+      get :index
+      expect(assigns(:pets)).to all(be_a Pet)
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
   end
 
-  it "has a collection of pets objects" do
-    get :index
-    expect(assigns(:pets)).to all(be_a Pet)
-  end
+  describe "Get # view " do
+    it "has a 200 status code" do
+      get :new
+      expect(response.status).to eq (200)
+    end
 
-  it "renders the index template" do
-    get :index
-    expect(response).to render_template("index")
+    it "create a new pet" do
+      get :new
+      expect(assigns(:pet)).to be_a_new Pet
+    end
+
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template(:new)
+    end
   end
 end
