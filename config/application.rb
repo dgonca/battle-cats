2ca5fb02f5ddc930ec7e4b17554dcfa5a936be30
mwmayerle <mwmayerle@gmail.com>
2017-08-18 19:02:39 -0500

    Fixed shield issue with Brendan after testing a server reboot

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,8 @@ require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
-require 'simplecov-shield'
-SimpleCov.formatter = SimpleCov::Formatter::ShieldFormatter
+
+#SimpleCov.formatter = SimpleCov::Formatter::ShieldFormatter COMMENTED OUT BY MATT
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
