c2f3937af6b47d248eb6da40b9d0657623df4995
Brendan O'Handley & Matt Mayerle <pair+bohandley.mwmayerle@devbootcamp.com>
2017-08-19 18:20:36 -0500

    add fight button, hide button, timeout through js, gather clicks to send to battles controller

---
+++ app/assets/javascripts/battle.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+  fightButtonListener();
+
+})
+var score;
+var clicks = 0;
+var fightButtonListener = function() {
+  $("#fight").one("click", function(){
+      var hideBoo = function() {
+        $("#smash").hide();
+        console.log("Smash hide function  " + clicks)
+        score = clicks;
+        $.ajax({
+          method: "put",
+          url: "/pets/battles"
+        })
+        //send dis to the controller
+      }
+
+      setTimeout(hideBoo, 2000)
+      $("#fight").replaceWith("<button id='smash' class='btn'>Hello</p>")
+
+      $("#smash").on("click", function(){
+          console.log("hey");
+          clicks += 1;
+          console.log("Smash on click function  " + clicks)
+          return clicks;
+      });
+      console.log("final end of function score  " + clicks)
+  });
+}
+
+
+// click fight fight button,
+// remove the fight button
+// start a loop that counts up once per second
+// once it reaches 10
