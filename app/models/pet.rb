182480755848039a6093c64a89a663a8a3247593
Saham Khozestani <email": null>
2017-08-19 12:54:54 -0500

    add image to index, styled with adding bootstrap and update .image_circle.css

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,5 @@
 class Pet < ApplicationRecord
-	
+
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   validates_attachment_size :avatar, :in => 0.megabytes..1.megabytes
