caf25f9fe1d7d40137ce1b7e0f7fc5377c87a224
Saham Khozestani <email": null>
2017-08-19 13:12:02 -0500

    add show path to index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :pets, only: [:index, :new, :create]
-  resources :users do 
+  resources :pets, only: [:index, :new, :create,:show]
+  resources :users do
   	resources :pets
   end
-  
+
   get "/sessions/new", to: "sessions#new"
   delete "/sessions", to: "sessions#destroy"
   post "/sessions", to: "sessions#create"
