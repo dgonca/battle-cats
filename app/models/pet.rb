6670ea93875ab417796da92f6375ff5c9235b070
Dominique Goncalves <email": null>
2017-08-20 17:50:42 -0500

    cuteness doesnt need to be present

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -9,7 +9,7 @@ class Pet < ApplicationRecord
 	has_many :battles, through: :pet_battles
 	belongs_to :owner, class_name: "User"
 
-	validates :name, :animal_type, :owner_id, :cuteness, presence: true
+	validates :name, :animal_type, :owner_id, presence: true
 
   def win_count
     wins = 0
