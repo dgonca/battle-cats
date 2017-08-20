24ddc3209b987b7f7d3c94f93b9950964dfffeda
mwmayerle <mwmayerle@gmail.com>
2017-08-20 17:01:11 -0500

    Re-added capybara feature tests and fixed session tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,17 +3,17 @@ require "rails_helper"
 RSpec.describe SessionsController, type: :controller do
   let!(:user) {User.create!(email: "matt@matt.com", password: "mattmatt")}
 
-  before(:each) do
-    session[:user_id] = 1
+  describe "while a user is logged out" do
+
+    it "has a 200 status code when going to new" do
+      get :new
+      expect(response.status).to eq (200)
+    end
+
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template("new")
+    end
   end
 
-  it "has a 200 status code when going to new" do
-    get :new
-    expect(response.status).to eq (200)
-  end
-
-  it "renders the new template" do
-    get :new
-    expect(response).to render_template("new")
-  end
 end
--- spec/features/navbar_spec.rb
+++ spec/features/navbar_spec.rb
@@ -1,70 +1,70 @@
-# require "spec_helper"
-# require "capybara/rspec"
+require "spec_helper"
+require "capybara/rspec"
 
-# feature "clicking on the nav bar while logged out" do
+feature "clicking on the nav bar while logged out" do
 
-# 	scenario "redirects to the sessions new page when the log in link is clicked" do
-# 		visit('/')
+	scenario "redirects to the sessions new page when the log in link is clicked" do
+		visit('/')
 
-# 		click_link('Log In')
-# 		expect(page).to have_current_path("/sessions/new")
-# 	end
+		click_link('Log In')
+		expect(page).to have_current_path("/sessions/new")
+	end
 
-# 	scenario "redirects to the registration page when the register link is clicked" do
-# 		visit('/')
-# 		click_link('Register')
-# 		expect(page).to have_current_path new_user_path
-# 	end
-# end
+	scenario "redirects to the registration page when the register link is clicked" do
+		visit('/')
+		click_link('Register')
+		expect(page).to have_current_path new_user_path
+	end
+end
 
-# feature "clicking on the navbar when logged in" do
+feature "clicking on the navbar when logged in" do
 
-# 	scenario "redirects to the pets index when the 'all pets' link is clicked" do
-# 		tom = User.create!(id: 1, email: "tom@tom.com", password: "tomtom")
+	scenario "redirects to the pets index when the 'all pets' link is clicked" do
+		tom = User.create!(id: 1, email: "tom@tom.com", password: "tomtom")
 
-# 		visit('/')
+		visit('/')
 
-# 		fill_in('Email', with: 'tom@tom.com')
-# 		fill_in('Password', with: 'tomtom')
+		fill_in('Email', with: 'tom@tom.com')
+		fill_in('Password', with: 'tomtom')
 
-# 		click_button('Log In')
+		click_button('Log In')
 
-# 		visit("/users/1")
+		visit("/users/1")
 
-# 		click_link("All Pets")
-# 		expect(page).to have_current_path("/pets")
-# 	end
+		click_link("All Pets")
+		expect(page).to have_current_path("/pets")
+	end
 
-# 	scenario "redirects to the user profile page when the 'user profile' link is clicked and a user is logged in" do
-# 		tom = User.create!(id: 1, email: "tom@tom.com", password: "tomtom")
+	scenario "redirects to the user profile page when the 'user profile' link is clicked and a user is logged in" do
+		tom = User.create!(id: 1, email: "tom@tom.com", password: "tomtom")
 
-# 		visit('/')
+		visit('/')
 
-# 		fill_in('Email', with: 'tom@tom.com')
-# 		fill_in('Password', with: 'tomtom')
+		fill_in('Email', with: 'tom@tom.com')
+		fill_in('Password', with: 'tomtom')
 
-# 		click_button('Log In')
+		click_button('Log In')
 
-# 		visit("/users/1")
+		visit("/users/1")
 
-# 		click_link("User Profile")
-# 		expect(page).to have_current_path("/users/1")
-# 	end
+		click_link("User Profile")
+		expect(page).to have_current_path("/users/1")
+	end
 
 
-# 	scenario "Logs the user out and redirects to root when 'log out' is clicked while a user is logged in" do
-# 		tom = User.create!(id: 1, email: "tom@tom.com", password: "tomtom")
+	scenario "Logs the user out and redirects to root when 'log out' is clicked while a user is logged in" do
+		tom = User.create!(id: 1, email: "tom@tom.com", password: "tomtom")
 
-# 		visit('/')
+		visit('/')
 
-# 		fill_in('Email', with: 'tom@tom.com')
-# 		fill_in('Password', with: 'tomtom')
+		fill_in('Email', with: 'tom@tom.com')
+		fill_in('Password', with: 'tomtom')
 
-# 		click_button('Log In')
+		click_button('Log In')
 
-# 		visit("/users/1")
+		visit("/users/1")
 
-# 		click_link("Logout")
-# 		expect(page).to have_current_path root_path
-# 	end
-# end
+		click_link("Logout")
+		expect(page).to have_current_path root_path
+	end
+end
