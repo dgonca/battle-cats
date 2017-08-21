c96824ab202960564f0c71c0e3f7664286d97fa1
dgonca <domstar1595@yahoo.com>
2017-08-21 15:49:23 -0500

    fix schema

--- db/migrate/20170821192150_add_indicies.rb
+++
@@ -1,7 +0,0 @@
-class AddIndicies < ActiveRecord::Migration[5.1]
-  def change
-    add_index :pets, :owner_id
-    add_index :pet_battles, :pet_id
-    add_index :pet_battles, :battle_id
-  end
-end
