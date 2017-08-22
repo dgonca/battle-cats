27d239fd5114ddac9e79127d281d2e2fadbc2a04
Saham Khozestani <email": null>
2017-08-22 10:29:15 -0500

    add 2 intial test for pet id and battle id in pet_battle_spec

--- spec/models/pet_battles_spec.rb
+++ spec/models/pet_battles_spec.rb
@@ -1,5 +1,20 @@
 require "rails_helper"
 
 describe PetBattle do
+  home = Dir.home
+  filepath = File.join(home, "/battle-cats/public/cat1.jpg")
 
+  let!(:user) {User.create!(email: "saham@att.net", password: "test")}
+  let!(:pet) {Pet.create(name: "Zee", animal_type: "Zee", bio: "a cute Zee", zipcode: "60192", owner: user, avatar: File.open(filepath))}
+
+  let!(:new_battle) {Battle.create!()}
+  let!(:pet_battle) {PetBattle.create(pet_id: pet.id, battle_id: new_battle.id)}
+
+  it "has a battle id" do
+      expect(pet_battle.battle_id).to eq(new_battle.id)
+  end
+
+  it "has a pet id" do
+      expect(pet_battle.pet_id).to eq(pet.id)
+  end
 end
