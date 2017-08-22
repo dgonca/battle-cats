7f5acc32d4cba5c6eef95732956394bf0c391d14
mwmayerle <mwmayerle@gmail.com>
2017-08-22 10:52:27 -0500

    Added destroy route to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-  resources :pets, only: [:index, :new, :create,:show]
+  resources :pets, only: [:index, :new, :create, :show, :destroy]
 
   resources :users do
   	resources :pets
