f552df7fafeaf3e0e2870705c7bcf590386db23f
dgonca <domstar1595@yahoo.com>
2017-08-24 14:33:17 -0500

    clean up ui of user-profile, extend time on battle

--- app/assets/javascripts/battle.js
+++ app/assets/javascripts/battle.js
@@ -36,7 +36,7 @@ $("#fight-button").one("click", function(){
 
    };
 
-   setTimeout(hideSmashButton, 2000);
+   setTimeout(hideSmashButton, 4000);
    replaceFightButton(); 
    clickAccumulator();  
    });
